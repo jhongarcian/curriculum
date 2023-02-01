9bfbaa0a1a3b9b7a8e4d19154f3e10f9a1c26ff1
John Garcia <jhongarcian@jhongarcian.local>
2023-01-31 21:37:22 -0600

    add exercises for jan31st

---
+++ homework/week3/jan31st/main.js
@@ -0,0 +1,16 @@
+// Write a function addr that takes two numbers as arguments and returns the sum of these two numbers when executed.
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(sum(2, 7));
+
+// Write a function customGreeting that takes two strings and returns a greeting. The first argument should be something like Hello or Hey and the second is the name of a person.
+
+function customGreeting(greeting, name) {
+    return `${greeting} ${name}`
+}
+
+console.log(customGreeting("Hello", "John"));
+
